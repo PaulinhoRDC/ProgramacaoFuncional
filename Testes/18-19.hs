e01ca23e30f20f2145355c1915454342ca8fe717
PaulinhoRDC <pauloricardodiasdacosta@gmail.com>
2021-12-27 21:19:25 +0000

    Teste 18-19 - working on it

--- Testes/18-19.hs
+++ Testes/18-19.hs
@@ -12,15 +12,49 @@ ocorre numa lista. Por exemplo, elemIndices 3 [1,2,3,4,3,2,3,4,5] corresponde a 
 Por exemplo, isSubsequenceOf [20,40] [10,20,30,40] corresponde a True enquanto que isSubsequenceOf [40,20] [10,20,30,40] corresponde a False.
 -}
 
+elemIndices :: Eq a => a -> [a] -> [Int]
+elemIndices x [] = []
+elemIndices x l = posIndice 0 x l
+
+posIndice :: Eq a => Int -> a -> [a] -> [Int]
+posIndice i x [] = []
+posIndice i x (h:t) |(x==h) = i : posIndice (i+1) x t
+                    |otherwise = posIndice (i+1) x t
+
+-- ----------------
+
+isSubsequenceOf :: Eq a => [a] -> [a] -> Bool
+isSubsequenceOf (x:xs) (y:ys) = if x==y then isSubsequenceOf xs ys
+                                        else isSubsequenceOf (x:xs) ys
+isSubsequenceOf [] _ = True
+isSubsequenceOf _ [] = False
+
+
 {-
 2. Considere o seguinte tipo para representar árvores binárias.
                       data BTree a = Empty | Node a (BTree a) (BTree a)
 
-(a)Defina a função  lookupAP:: Ord a => a-> BTree (a,b)-> Maybe b que generaliza a função
+(a)Defina a função  lookupAP :: Ord a => a-> BTree (a,b)-> Maybe b que generaliza a função
 lookup para árvores binárias de procura.
 
 (b) Defina a função zipWithBT :: (a -> b -> c) -> BTree a -> BTree b -> BTree c que generaliza a função zipWith para árvores binárias.
 -}
+
+data BTree a = Empty | Node a (BTree a) (BTree a)
+
+lookupAP :: Ord a => a -> BTree (a,b) -> Maybe b
+lookupAp x Empty = Nothing
+lookupAP x (Node (a,b) esq dir) |(x==a) = Just b
+                                |(x<a) = lookupAP x esq
+                                |otherwise = lookupAP x dir
+
+
+-- ----------------
+
+zipWithBT :: (a -> b -> c) -> BTree a -> BTree b -> BTree c
+zipWithBT _ Empty _ = Empty
+zipWithBT _ _ Empty = Empty
+zipWithBT f (Node a esq dir) (Node a2 esq2 dir2) = Node (f a a2) (zipWithBT f esq esq2) (zipWithBT f dir dir2)
 
 {-
 3. Defina a função digitAlpha :: String -> (String,String), que dada uma string,
@@ -29,12 +63,24 @@ Implemente a função de modo a fazer uma única travessia da string.
 Sugestão: pode usar as funções isDigit, isAlpha :: Char -> Bool.
 -}
 
+digitAlpha :: String -> (String,String)
+digitAlpha [] = ([],[])
+digitAlpha (x:xs) | (isDigit x) = (x:num , letr)
+                  | (isAlpha x) = (num , x:letr)
+                  | otherwise = (num , letr)
+
+            where (num,letr) = digitAlpha xs
+
+
+digitAlpha' :: String -> (String,String)
+digitAlpha' l = foldr (\x (ds,as) -> if isDigit x then (x:ds,as) else if isAlpha x then (ds,x:as) else (ds,as)) ([],[]) l
+
 {-
 4. Considere o seguinte tipo de dados para representar uma sequência em que os elementos podem ser acrecentados à esquerda (Cons)
 ou por concatenação de duas sequências (App).
                                                 data Seq a = Nil | Cons a (Seq a) | App (Seq a) (Seq a)
 
-(a) Defina a função firstSeq :: Seq a -> a que recebe uma sequência não vazia e devolve o seu primeiro elemento.
+(a) Defina a função firstSeq :: Seq a -> a ,que recebe uma sequência não vazia e devolve o seu primeiro elemento.
 
 (b) Defina a função dropSeq :: Int -> Seq a -> Seq a, tal que dropSeq n s elimina os n primeiros elementos da sequência s.
  A função deve manter a estrutura da sequência.
@@ -44,6 +90,29 @@ ou por concatenação de duas sequências (App).
        > App (Cons 1 Nil) (App (Cons 7 (Cons 5 Nil)) (Cons 3 Nil))
        <<1,7,5,3>>
 -}
+
+data Seq a = Nil | Cons a (Seq a) | App (Seq a) (Seq a)
+
+firstSeq :: Seq a -> a
+firstSeq (Cons x s) = x
+firstSeq (App (Nil) s) = firstSeq s
+firstSeq (App s (Nil)) = firstSeq s
+
+-- ----------------
+
+dropSeq :: Int -> Seq a -> Seq a
+dropSeq _ (Nil) = Nil
+dropSeq n (Cons a s) = dropSeq (n - 1) s
+dropSeq n (App s1 s2) | n > nx = dropSeq (n-nx) s2
+                      | n == nx = s2
+                      | otherwise = (App (dropSeq n s1) s2)
+     where nx = contaCons s1
+
+contaCons :: Seq a -> Int
+contaCons Nil = 0
+contaCons (Cons _ s) = 1 + contaCons s
+contaCons (App s1 s2) = contaCons s1 + contaCons s2
+
 
 {-
 5. Considere a sequinte definição para representar matrizes: type Mat a = [[a]]
