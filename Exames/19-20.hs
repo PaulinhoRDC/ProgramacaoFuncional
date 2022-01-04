1300aeffb80936248714c01290dfe2c420355ee7
PaulinhoRDC <pauloricardodiasdacosta@gmail.com>
2022-01-04 16:10:13 +0000

    Exame 19-20

--- Exames/19-20.hs
+++ Exames/19-20.hs
@@ -1,16 +1,28 @@
 module Exame1920 where
 
 import  Data.Char
+import System.Random
+import System.IO
+import System.IO.Error
 
 {-
 1. Apresente uma definição recursiva das seguintes funções (pré-definidas) sobre listas:
 
-(a) inits:: [a] -> [[a]] que calcula a lista dos prefixos de uma lista.
+(a) inits :: [a] -> [[a]] que calcula a lista dos prefixos de uma lista.
 Por exemplo, inits [11,21,13] corresponde a [[],[11],[11,21],[11,21,13]].
 
 (b) isPrefixOf:: Eq a => [a] -> [a] -> Bool que testa se uma lista é prefixo de outra.
 Por exemplo, isPrefixOf [10,20] [10,20,30] corresponde a True enquanto que isPrefixOf [10,30] [10,20,30] corresponde a False.
 -}
+
+inits :: [a] -> [[a]]
+inits [] = [[]]
+inits l = inits (init l) ++ [l]
+
+isPrefixOf:: Eq a => [a] -> [a] -> Bool
+isPrefixOf (x:xs) (y:ys) = (x==y) && isPrefixOf xs ys
+isPrefixOf [] _ = True
+isPrefixOf _ [] = False
 
 {-
 2. Considere o seguinte tipo para representar árvores binárias.
@@ -23,6 +35,21 @@ Por exemplo, isPrefixOf [10,20] [10,20,30] corresponde a True enquanto que isPre
  (b) Defina a função path :: [Bool] -> BTree a -> [a], que dado um caminho (False corresponde a esquerda e True a direita)
  e uma árvore, dá a lista com a informação dos nodos por onde esse caminho passa.
 -}
+
+data BTree a = Empty | Node a (BTree a) (BTree a)
+         deriving Show
+
+folhas :: BTree a -> Int
+folhas Empty = 0
+folhas (Node a Empty Empty) = 1
+folhas (Node a esq dir) = (folhas esq) + (folhas dir)
+
+path :: [Bool] -> BTree a -> [a]
+path _ Empty = []
+path [] (Node a _ _) = [a]
+path (x:xs) (Node a esq dir) = if x
+  then a: path xs dir
+  else a: path xs esq
 
 {-
 3. Uma representação possével de polimómios é pela sequência dos coeficientes - têm que se armazenar também os coeficientes nulos
@@ -41,6 +68,35 @@ pois será a posição do coeficiente na lista que dará o grau do monómio.
 (c) Defina a operação soma :: Polinomio -> Polinomio -> Polinomio de adição de polinómios.
 -}
 
+type Polinomio = [Coeficiente]
+type Coeficiente = Float
+
+
+-- ---------------- a)
+
+valor :: Polinomio -> Float -> Float
+valor [] _ = 0
+valor (h:t) x = contaGrau (h:t) x 0
+
+contaGrau :: Polinomio -> Float -> Int -> Float
+contaGrau [] _ _ = 0
+contaGrau (h:t) x g = (h*(x^ fromIntegral(g)) + contaGrau t x (g+1) )
+
+-- OUUUUUUU
+
+valor' :: Polinomio -> Float -> Float
+valor' p x = sum (zipWith (\a b -> b * x ^ a) [0..] p)
+
+-- ---------------- b)
+
+deriv :: Polinomio -> Polinomio
+deriv = tail . (zipWith (*) [0..])    -- deriv p = tail . (zipWith (*) [0..] p)
+
+-- ---------------- c)
+
+soma :: Polinomio -> Polinomio -> Polinomio
+soma p1 p2 = zipWith (+) p1 p2
+
 {-
 4. Considere a seguinte definição para representar matrizes: type Mat a = [[a]].
 
@@ -50,7 +106,7 @@ ex = [[1,4,3,2,5], [6,7,8,9,0], [3,5,4,9,1]] representa a matriz abaixo desenhad
 que recebe uma lista de inteiros s e uma linha l, e produz a lista de segmentos contíguos de l de comprimento indicado em s.
     Por exemplo, quebraLinha [2,3] [1,4,3,2,5] == [[1,4],[3,2,5]].
 
-(b) Defina a função fragmenta :: [Int] -> [Int] -> Mat a -> [Mat a] que recebe duas lista de inteiros
+(b) Defina a função fragmenta :: [Int] -> [Int] -> Mat a -> [Mat a] ,que recebe duas lista de inteiros
 (com a partição das linhas e das colunas) e uma matriz, e produz a lista de (sub)-matrizes de acordo com essa partição.
     Por exemplo, fragmenta [2,1] [2,3] ex == [ [[1,4],[6,7]], [[3,2,5],[8,9,0]], [[3,5]], [[4,9,1]] ].
 
@@ -58,3 +114,35 @@ que recebe uma lista de inteiros s e uma linha l, e produz a lista de segmentos 
  geraMat (x,y) (a,b) gera aleatoriamente uma matriz com x linhas e y colunas, cujos valores estão compreendidos entre a e b.
     Sugestão: use a função randomRIO :: Random a => (a,a) -> IO a.
 -}
+
+type Mat a = [[a]]
+
+-- ---------------- a)
+
+quebraLinha :: [Int] -> [a] -> [[a]]
+quebraLinha [] _ = []
+quebraLinha (h:t) l = take h l : quebraLinha t (drop h l)
+
+-- ---------------- b)
+
+fragmenta :: [Int] -> [Int] -> Mat a -> [Mat a]
+fragmenta [] _ _ = []
+fragmenta (h:t) pc m = quebraLinhas pc (take h m) ++ fragmenta t pc (drop h m)
+
+quebraLinhas :: [Int] -> Mat a -> [Mat a]
+quebraLinhas [] _ = []
+quebraLinhas (h:t) m = map (take h) m : quebraLinhas t (map (drop h) m)
+
+-- ---------------- c)
+
+geraLinha :: Int -> (Int, Int) -> IO [Int]
+geraLinha x r |(x==0) = return []
+              |otherwise = do t <- geraLinha (x-1) r
+                              n <- randomRIO r
+                              return (n : t)
+
+geraMat :: (Int,Int) -> (Int,Int) -> IO (Mat Int)
+geraMat (x,y) r |(x==0) = return []
+                |otherwise = do m <- geraMat (x-1,y) r
+                                l <- geraLinha y r
+                                return ( l : m )
